b0ed20ce81e32cca495b63efd0e5fc31222df4e2
Dumindu Madunuwan <dumindu.madunuwan@gmail.com>
2026-06-27 14:18:43 +0800

    format struct-tag

--- model/book.go
+++ model/book.go
@@ -9,42 +9,13 @@ import (
 type (
 	Books []*Book
 	Book  struct {
-		ID            uuid.UUID `gorm:"primarykey"`
-		Title         string
-		Author        string
-		PublishedDate time.Time
-		ImageURL      string
-		Description   string
-		CreatedAt     time.Time `gorm:"autoCreateTime"`
-		UpdatedAt     time.Time `gorm:"autoUpdateTime"`
+		ID            uuid.UUID `json:"id" gorm:"primarykey"`
+		Title         string    `json:"title"`
+		Author        string    `json:"author"`
+		PublishedDate time.Time `json:"published_date,format:'2006-01-02'"`
+		ImageURL      string    `json:"image_url"`
+		Description   string    `json:"description"`
+		CreatedAt     time.Time `json:"created_at,format:RFC3339" gorm:"autoCreateTime"`
+		UpdatedAt     time.Time `json:"updated_at,format:RFC3339" gorm:"autoUpdateTime"`
 	}
 )
-
-type BookDTO struct {
-	ID            string `json:"id"`
-	Title         string `json:"title"`
-	Author        string `json:"author"`
-	PublishedDate string `json:"published_date"`
-	ImageURL      string `json:"image_url"`
-	Description   string `json:"description"`
-}
-
-func (b *Book) ToDTO() *BookDTO {
-	return &BookDTO{
-		ID:            b.ID.String(),
-		Title:         b.Title,
-		Author:        b.Author,
-		PublishedDate: b.PublishedDate.Format("2006-01-02"),
-		ImageURL:      b.ImageURL,
-		Description:   b.Description,
-	}
-}
-
-func (bs Books) ToDTO() []*BookDTO {
-	resp := make([]*BookDTO, len(bs))
-	for i, v := range bs {
-		resp[i] = v.ToDTO()
-	}
-
-	return resp
-}
