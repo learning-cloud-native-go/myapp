b619708f57e281b93765c8e2e911edd1c04a5c7b
Dumindu Madunuwan <dumindu.madunuwan@gmail.com>
2026-08-23 00:57:18 +0800

    Clean log

--- pkg/middleware/requestlog/handler.go
+++ pkg/middleware/requestlog/handler.go
@@ -22,75 +22,62 @@ import (
 	"net/http"
 	"time"
 
-	"myapp/pkg/ctxutil"
-	"myapp/pkg/logger"
+	"github.com/rs/zerolog/hlog"
 )
 
-type Handler struct {
-	handler http.Handler
-	logger  *logger.Logger
-}
+func NewHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
 
-func NewHandler(h http.HandlerFunc, l *logger.Logger) *Handler {
-	return &Handler{
-		handler: h,
-		logger:  l,
-	}
-}
+		le := &logEntry{
+			ReceivedTime:      start,
+			RequestMethod:     r.Method,
+			RequestURL:        r.URL.String(),
+			RequestHeaderSize: headerSize(r.Header),
+			UserAgent:         r.UserAgent(),
+			Referer:           r.Referer(),
+			Proto:             r.Proto,
+			RemoteIP:          ipFromHostPort(r.RemoteAddr),
+		}
 
-func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
+		if addr, ok := r.Context().Value(http.LocalAddrContextKey).(net.Addr); ok {
+			le.ServerIP = ipFromHostPort(addr.String())
+		}
+		r2 := new(http.Request)
+		*r2 = *r
+		rcc := &readCounterCloser{r: r.Body}
+		r2.Body = rcc
+		w2 := &responseStats{w: w}
 
-	le := &logEntry{
-		RequestID:         ctxutil.RequestID(r.Context()),
-		ReceivedTime:      start,
-		RequestMethod:     r.Method,
-		RequestURL:        r.URL.String(),
-		RequestHeaderSize: headerSize(r.Header),
-		UserAgent:         r.UserAgent(),
-		Referer:           r.Referer(),
-		Proto:             r.Proto,
-		RemoteIP:          ipFromHostPort(r.RemoteAddr),
-	}
+		next.ServeHTTP(w2, r2)
 
-	if addr, ok := r.Context().Value(http.LocalAddrContextKey).(net.Addr); ok {
-		le.ServerIP = ipFromHostPort(addr.String())
-	}
-	r2 := new(http.Request)
-	*r2 = *r
-	rcc := &readCounterCloser{r: r.Body}
-	r2.Body = rcc
-	w2 := &responseStats{w: w}
-
-	h.handler.ServeHTTP(w2, r2)
-
-	le.Latency = time.Since(start)
-	if rcc.err == nil && rcc.r != nil {
-		// If the handler hasn't encountered an error in the Body (like EOF),
-		// then consume the rest of the Body to provide an accurate rcc.n.
-		io.Copy(io.Discard, rcc)
-	}
-	le.RequestBodySize = rcc.n
-	le.Status = w2.code
-	if le.Status == 0 {
-		le.Status = http.StatusOK
-	}
-	le.ResponseHeaderSize, le.ResponseBodySize = w2.size()
-	h.logger.Info().
-		Str("request_id", le.RequestID).
-		Time("received_time", le.ReceivedTime).
-		Str("method", le.RequestMethod).
-		Str("url", le.RequestURL).
-		Int64("header_size", le.RequestHeaderSize).
-		Int64("body_size", le.RequestBodySize).
-		Str("agent", le.UserAgent).
-		Str("referer", le.Referer).
-		Str("proto", le.Proto).
-		Str("remote_ip", le.RemoteIP).
-		Str("server_ip", le.ServerIP).
-		Int("status", le.Status).
-		Int64("resp_header_size", le.ResponseHeaderSize).
-		Int64("resp_body_size", le.ResponseBodySize).
-		Dur("latency", le.Latency).
-		Msg("")
+		le.Latency = time.Since(start)
+		if rcc.err == nil && rcc.r != nil {
+			// If the handler hasn't encountered an error in the Body (like EOF),
+			// then consume the rest of the Body to provide an accurate rcc.n.
+			io.Copy(io.Discard, rcc)
+		}
+		le.RequestBodySize = rcc.n
+		le.Status = w2.code
+		if le.Status == 0 {
+			le.Status = http.StatusOK
+		}
+		le.ResponseHeaderSize, le.ResponseBodySize = w2.size()
+		hlog.FromRequest(r).Info().
+			Time("received_time", le.ReceivedTime).
+			Str("method", le.RequestMethod).
+			Str("url", le.RequestURL).
+			Int64("header_size", le.RequestHeaderSize).
+			Int64("body_size", le.RequestBodySize).
+			Str("agent", le.UserAgent).
+			Str("referer", le.Referer).
+			Str("proto", le.Proto).
+			Str("remote_ip", le.RemoteIP).
+			Str("server_ip", le.ServerIP).
+			Int("status", le.Status).
+			Int64("resp_header_size", le.ResponseHeaderSize).
+			Int64("resp_body_size", le.ResponseBodySize).
+			Dur("latency", le.Latency).
+			Msg("")
+	})
 }
